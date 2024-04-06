13ad8477ed40a8ccc566e77c2bc7af0009fbe872
Jacob Biros <jakebiros@gmail.com>
2024-04-06 17:44:36 +0900

    Add primary file

--- src/gameboy.cpp
+++ src/gameboy.cpp
@@ -1,5 +1,14 @@
+#include "disassembler.h"
 
+#include <iostream>
+#include <string>
 
 int main() {
     
+    Disassembler disassembler;
+
+    std::string value = "0x00";
+
+    std::cout << disassembler.interpret(value) << std::endl;
+
 }
