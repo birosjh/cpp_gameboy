01e4b7fa181263df831b3923c5d382131d823a77
Jacob Biros <jakebiros@gmail.com>
2024-07-03 19:03:07 +0900

    Fix param names to match defintions

--- src/device/operations/increment.h
+++ src/device/operations/increment.h
@@ -8,13 +8,13 @@
 // Increment/Decrement Operation Functions
 
 namespace INC {
-    uint16_t single(CPU& cpu, std::string double_register);
+    uint16_t single(CPU& cpu, std::string in_register);
     uint16_t pair(CPU& cpu, std::string double_register);
     uint16_t address(CPU& cpu, MemoryBus& memory_bus, std::string double_register);
 }
 
 namespace DEC {
-    uint16_t single(CPU& cpu, std::string double_register);
+    uint16_t single(CPU& cpu, std::string in_register);
     uint16_t pair(CPU& cpu, std::string double_register);
     uint16_t address(CPU& cpu, MemoryBus& memory_bus, std::string double_register);
 }
