7f2a9003d42bca5b0797263d575b7021bb1b3438
Jacob Biros <jakebiros@gmail.com>
2025-01-11 20:27:41 +0900

    Use overloading instead of long function names for functions that are almost the same

--- src/device/operations/load.cpp
+++ src/device/operations/load.cpp
@@ -18,11 +18,28 @@ uint16_t LD::single_from_single(CPU& cpu, Register in_register, Register load_re
 
 uint16_t LD::single_from_address(CPU& cpu, MemoryBus& memory_bus, Register in_register, DoubleRegister address_register) {
 
-    auto address = cpu.double_registers[address_register];
+    auto address = cpu.double_register(address_register);
 
     auto value_at_address = memory_bus.read_from_memory(address);
 
     cpu.registers[in_register] = value_at_address;
+
+    return cpu.pc() + 1;
+}
+
+uint16_t LD::single_from_address(CPU& cpu, MemoryBus& memory_bus, Register in_register, DoubleRegister address_register, Modifier modifier) {
+
+    auto address = cpu.double_register(address_register);
+
+    auto value_at_address = memory_bus.read_from_memory(address);
+
+    cpu.registers[in_register] = value_at_address;
+
+    if(modifier == Inc) {
+        INC::pair(cpu, address_register);
+    } else {
+        DEC::pair(cpu, address_register);
+    }
 
     return cpu.pc() + 1;
 }
@@ -36,7 +53,7 @@ uint16_t LD::single_from_value(CPU& cpu, Register in_register, uint8_t value) {
 
 uint16_t LD::to_address_from_single(CPU& cpu, MemoryBus& memory_bus, DoubleRegister address_register, Register load_register) {
 
-    auto address = cpu.double_registers[address_register];
+    auto address = cpu.double_register(address_register);
 
     auto value = cpu.registers[load_register];
 
@@ -47,29 +64,33 @@ uint16_t LD::to_address_from_single(CPU& cpu, MemoryBus& memory_bus, DoubleRegis
 
 uint16_t LD::to_address_from_value(CPU& cpu, MemoryBus& memory_bus, DoubleRegister address_register, uint8_t value) {
 
-    auto address = cpu.double_registers[address_register];
+    auto address = cpu.double_register(address_register);
 
     memory_bus.write_to_memory(address, value);
 
     return cpu.pc() + 1;
 }
 
-uint16_t LD::to_address_from_single_with_increment(CPU& cpu, MemoryBus& memory_bus, DoubleRegister address_register, Register load_register) {
+uint16_t LD::to_address_from_single(CPU& cpu, MemoryBus& memory_bus, DoubleRegister address_register, Register load_register, Modifier modifier) {
 
-    auto address = cpu.double_registers[address_register];
+    auto address = cpu.double_register(address_register);
 
     auto value = cpu.registers[load_register];
 
     memory_bus.write_to_memory(address, value);
 
-    INC::pair(cpu, address)
+    if(modifier == Inc) {
+        INC::pair(cpu, address_register);
+    } else {
+        DEC::pair(cpu, address_register);
+    }
 
     return cpu.pc() + 1;
 }
 
 uint16_t LD::double_from_value(CPU& cpu, DoubleRegister in_register, uint16_t value) {
 
-    cpu.double_registers(in_register, value);
+    cpu.double_register(in_register, value);
 
     return cpu.pc() + 1;
 }
--- src/device/operations/load.h
+++ src/device/operations/load.h
@@ -9,13 +9,18 @@
 
 // Load Operation Functions
 
+enum Modifier {
+    Inc, Dec
+}
+
 namespace LD {
     uint16_t single_from_single(CPU& cpu, Register in_register, Register load_register);
     uint16_t single_from_address(CPU& cpu, MemoryBus& memory_bus, Register in_register, DoubleRegister address_register);
+    uint16_t single_from_address(CPU& cpu, MemoryBus& memory_bus, Register in_register, DoubleRegister address_register, Modifier modifier);
     uint16_t single_from_value(CPU& cpu, Register Register, uint8_t value);
     uint16_t to_address_from_single(CPU& cpu, MemoryBus& memory_bus, DoubleRegister address_register, Register load_register);
+    uint16_t to_address_from_single(CPU& cpu, MemoryBus& memory_bus, DoubleRegister address_register, Register load_register, Modifier modifier);
     uint16_t to_address_from_value(CPU& cpu, MemoryBus& memory_bus, DoubleRegister address_register, uint8_t value);
-    uint16_t to_address_from_single_with_increment(CPU& cpu, MemoryBus& memory_bus, DoubleRegister address_register, Register load_register);
 
     uint16_t double_from_value(CPU& cpu, DoubleRegister in_register, uint16_t value);
 }
