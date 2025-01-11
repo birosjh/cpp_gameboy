07a61a32ff387d8a6682ac52b984809c53905b9f
Jacob Biros <jakebiros@gmail.com>
2025-01-11 11:40:05 +0900

    Update function calls and add new logic

--- src/device/operations/load.cpp
+++ src/device/operations/load.cpp
@@ -1,6 +1,7 @@
 #include <exception>
 
 #include "load.h"
+#include "increment.h"
 
 // -----------------------------------------------
 // Load Commands
@@ -15,8 +16,9 @@ uint16_t LD::single_from_single(CPU& cpu, Register in_register, Register load_re
 }
 
 
-uint16_t LD::single_from_address(CPU& cpu, MemoryBus& memory_bus, Register in_register, uint16_t address) {
+uint16_t LD::single_from_address(CPU& cpu, MemoryBus& memory_bus, Register in_register, DoubleRegister address_register) {
 
+    auto address = cpu.double_registers[address_register];
 
     auto value_at_address = memory_bus.read_from_memory(address);
 
@@ -32,7 +34,9 @@ uint16_t LD::single_from_value(CPU& cpu, Register in_register, uint8_t value) {
     return cpu.pc() + 1;
 }
 
-uint16_t LD::to_address_from_single(CPU& cpu, MemoryBus& memory_bus, uint16_t address, Register load_register) {
+uint16_t LD::to_address_from_single(CPU& cpu, MemoryBus& memory_bus, DoubleRegister address_register, Register load_register) {
+
+    auto address = cpu.double_registers[address_register];
 
     auto value = cpu.registers[load_register];
 
@@ -41,9 +45,31 @@ uint16_t LD::to_address_from_single(CPU& cpu, MemoryBus& memory_bus, uint16_t ad
     return cpu.pc() + 1;
 }
 
+uint16_t LD::to_address_from_value(CPU& cpu, MemoryBus& memory_bus, DoubleRegister address_register, uint8_t value) {
+
+    auto address = cpu.double_registers[address_register];
+
+    memory_bus.write_to_memory(address, value);
+
+    return cpu.pc() + 1;
+}
+
+uint16_t LD::to_address_from_single_with_increment(CPU& cpu, MemoryBus& memory_bus, DoubleRegister address_register, Register load_register) {
+
+    auto address = cpu.double_registers[address_register];
+
+    auto value = cpu.registers[load_register];
+
+    memory_bus.write_to_memory(address, value);
+
+    INC::pair(cpu, address)
+
+    return cpu.pc() + 1;
+}
+
 uint16_t LD::double_from_value(CPU& cpu, DoubleRegister in_register, uint16_t value) {
 
-    cpu.double_register(in_register, value);
+    cpu.double_registers(in_register, value);
 
     return cpu.pc() + 1;
 }
--- src/device/operations/load.h
+++ src/device/operations/load.h
@@ -11,9 +11,11 @@
 
 namespace LD {
     uint16_t single_from_single(CPU& cpu, Register in_register, Register load_register);
-    uint16_t single_from_address(CPU& cpu, MemoryBus& memory_bus, Register in_register, uint16_t address);
+    uint16_t single_from_address(CPU& cpu, MemoryBus& memory_bus, Register in_register, DoubleRegister address_register);
     uint16_t single_from_value(CPU& cpu, Register Register, uint8_t value);
-    uint16_t to_address_from_single(CPU& cpu, MemoryBus& memory_bus, uint16_t address, Register load_register);
+    uint16_t to_address_from_single(CPU& cpu, MemoryBus& memory_bus, DoubleRegister address_register, Register load_register);
+    uint16_t to_address_from_value(CPU& cpu, MemoryBus& memory_bus, DoubleRegister address_register, uint8_t value);
+    uint16_t to_address_from_single_with_increment(CPU& cpu, MemoryBus& memory_bus, DoubleRegister address_register, Register load_register);
 
     uint16_t double_from_value(CPU& cpu, DoubleRegister in_register, uint16_t value);
 }
