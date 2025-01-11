fccd33db99702df41051de3602735e6aaefd4bb1
Jacob Biros <jakebiros@gmail.com>
2025-01-11 11:40:52 +0900

    Update signatures and fix logic

--- src/device/operations/jump.cpp
+++ src/device/operations/jump.cpp
@@ -16,10 +16,24 @@ uint16_t JP::to_address_in_register(CPU& cpu, DoubleRegister in_register) {
     return cpu.pc() + 1;
 }
 
-uint16_t JP::to_address_if_flag(CPU& cpu, Flag flag, uint16_t address) {
+uint16_t JP::to_address_if_flag_state(CPU& cpu, FlagState flag_state, uint16_t address) {
 
-    if (cpu.flags[flag]) {
-        JP::to_address(cpu, address);
+    if (flag_state == ZeroOn) {
+        if (cpu.flags[Zero]) {
+            return JP::to_address(cpu, address);
+        }
+    } else if(flag_state == ZeroOff) {
+        if (!cpu.flags[Zero]) {
+            return JP::to_address(cpu, address);
+        }
+    } else if(flag_state == CarryOn) {
+        if (cpu.flags[Carry]) {
+            return JP::to_address(cpu, address);
+        }
+    } else if(flag_state == CarryOff) {
+        if (!cpu.flags[Carry]) {
+            return JP::to_address(cpu, address);
+        }
     }
 
     return cpu.pc() + 3;
@@ -40,6 +54,15 @@ uint16_t JR::by_adding(CPU& cpu, uint8_t value) {
 uint16_t JR::by_adding_if_flag(CPU& cpu, Flag flag, uint8_t value) {
 
     if (cpu.flags[flag]) {
+        JR::by_adding(cpu, value);
+    }
+
+    return cpu.pc() + 2;
+}
+
+uint16_t JR::by_adding_if_not_flag(CPU& cpu, Flag flag, uint8_t value) {
+
+    if (!cpu.flags[flag]) {
         JR::by_adding(cpu, value);
     }
 
--- src/device/operations/jump.h
+++ src/device/operations/jump.h
@@ -12,12 +12,13 @@
 namespace JP {
     uint16_t to_address(CPU& cpu, uint16_t address);
     uint16_t to_address_in_register(CPU& cpu, DoubleRegister in_register);
-    uint16_t to_address_if_flag(CPU& cpu, Flag flag, uint16_t address);
+    uint16_t to_address_if_flag_state(CPU& cpu, FlagState flag_state, uint16_t address);
 }
 
 namespace JR {
     uint16_t by_adding(CPU& cpu, uint8_t value);
     uint16_t by_adding_if_flag(CPU& cpu, Flag flag, uint8_t value);
+    uint16_t by_adding_if_not_flag(CPU& cpu, Flag flag, uint8_t value)
 }
 
 #endif // JUMP_H
