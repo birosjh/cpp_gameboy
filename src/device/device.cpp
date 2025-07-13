322ebafe3736596358795ce9e36db860956e6b40
Jacob Biros <jakebiros@gmail.com>
2025-07-13 22:24:31 +0900

    Add a method to print the current command

--- src/device/device.cpp
+++ src/device/device.cpp
@@ -1,13 +1,11 @@
+#include <iomanip>
 #include "device.h"
-
-
 
 Device::Device(Cartridge cartridge) :
     current_game(cartridge),
     memory_bus(),
     cpu()
 {
-
     // Set CPU Program Counter
     cpu.pc(0);
 }
@@ -15,12 +13,21 @@ Device::Device(Cartridge cartridge) :
 void Device::run() {
     uint8_t code;
 
-    while(true) {
+    std::cout << "Program Counter: " << cpu.pc() << std::endl;
+
+    code = memory_bus.get_next_in_memory(cpu);
+
+    while(code) {
+
+        std::cout << "0x" << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(code) << std::endl;
+
+        // Execute opcode
+        auto new_location = execute(code);
+
+        std::cout << "Program Counter: " << cpu.pc() << std::endl;
 
         // Get next opcode
         code = memory_bus.get_next_in_memory(cpu);
-        // Execute opcode
-        auto new_location = execute(code);
     }
 }
 
